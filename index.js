bdbfb761f1ccbca400a48bdc0f32f9745efa2ff2
Luis <luis.guariglia@gmail.com>
2020-07-04 14:53:13 -0300

    avanze vastante

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var app = require('express')();
+var express=require("express");
+var app=express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+
+app.use("/build", express.static(__dirname + '/build'));
+app.use("/midi", express.static(__dirname + '/node_modules/midiconvert/build'));
+app.use("/images", express.static(__dirname + './images'));
+app.use("/audio/Salamander", express.static(__dirname + '/audio/Salamander'));
+app.use("/audio/string_ensemble", express.static(__dirname + '/audio/string_ensemble'));
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
