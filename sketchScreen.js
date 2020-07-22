1eb4550293fa35e6d869df95f876666389021c98
Luis <luis.guariglia@gmail.com>
2020-07-22 14:08:21 -0300

    screen mejor con cuadrados de c olores

--- sketchScreen.js
+++ sketchScreen.js
@@ -1,76 +1,112 @@
-let x, y;
-let player;
-let to;
-let cont=0;
-var posY=0;
-var altura=0;
-
+let system;
+let trigger=0;
+let maximo=100;
+let timer=0;
 function setup() {
-  altura=windowHeight;
-  createCanvas(windowWidth, altura);
-  noStroke();
-  x = width / 2;
-  y = height/2;
-  player=color(66, 200, 245);
-  from=color(66, 200, 245);
-  to=color(66, 200, 245);
+  createCanvas(windowWidth, windowHeight);
+  system = new ParticleSystem(createVector(width / 2, height/2));
+  //noStroke();
+  background(0);
 }
 function setY(num) {
-  posY=10+(altura-(num*altura)/107);
+  //var octava =document.getElementById("octava").value;
+ // notaFinal=num+(12*octava);
+
+ // posY=10+(altura-(notaFinal*altura)/107);
+ trigger=maximo;
 }
 function draw() {
   clear();
-  //posY=mouseY;
-  if(posY>height-150){
-    posY=height-150;
-  };
-  if(posY<10){
-    posY=10;
-  };
-  if (cont==100) {
-      cont=0;
-      to=color( random(255),  random(255),  random(255));      
+  rotate(cos(timer*0.005)/4);
+  if(trigger>=maximo){  
+    
+  var color=[random(0, 240), random(0, 240), random(0, 240)];
+  system.addParticle(color);
+   
+  trigger=0;
+  maximo=random(0, 100);
+ 
   }else{
-    cont++;
+  //trigger++;
   }
-  player = lerpColor(player, to, 0.1);
-  fill(player);
-  bezier(
-      x,  
-      y-200+posY,
-      
-      x-200-(posY*0.5),
-      y+240,
-      x+200+(posY*0.5),
-      y+240,
-      
-      x,
-      y-200+posY
-  );
+  timer++;
   
-  
-  arc(x-25, height-90, 20+posY*0.1, 180, 0, PI);
-  arc(x+25, height-90, 20+posY*0.1, 180, 0, PI);
-  //ellipse(x+25, y+150, 20, 100);
-  //ellipse(x-25, y+150, 20, 100);
-  
-  fill(color( 255,  255,  255)); 
-  ellipse(x+25, y+50+posY/4, 20, 20);
-  ellipse(x-25, y+50+posY/4, 20, 20);
-  
-  //fill(color( 0,  0,  0));
-  //ellipse(x+25, y+50+posY/4, 10, 10);
-  //ellipse(x-25, y+50+posY/4, 10, 10);
-  
-  fill(color( 0,  0,  0));
-  ellipse(x, y+100+posY/5, 30, 20+(height-posY)*0.08);
-  
-  
-  fill(player);
-  angle1 = (mouseX / float(width) - 0.5) * -TWO_PI;
-  angle2 = (mouseY / float(height) - 0.5) * PI;
-  ellipse(x+100-(posY*0.1), y+70-posY/15, 20, 20);
-  ellipse(x-100+(posY*0.1), y+70-posY/15, 20, 20);
-
-  
+  system.run();
 }
+
+// A simple Particle class
+let Particle = function(position,colores) {
+  this.acceleration = createVector(cos(timer*0.005)*0.0001, cos(timer*0.005)*0.001);
+  //this.velocity = createVector(cos(timer*0.005)/4, sin(timer*0.005)/4);
+  this.velocity = createVector(0, 0);
+  this.position = position.copy();
+  this.lifespan = 2000;
+  this.colores=colores;
+};
+
+Particle.prototype.run = function() {
+  this.update();
+  this.display();
+};
+
+// Method to update position
+Particle.prototype.update = function(){
+  this.velocity.add(this.acceleration);
+  this.position.add(this.velocity);
+  this.lifespan -= 4
+};
+
+// Method to display
+Particle.prototype.display = function() {
+  stroke(0, this.lifespan);
+  //strokeWeight(2);
+
+  //fill(127, this.lifespan);
+  //ellipse(this.position.x, this.position.y, 12, 12);
+  var tam=this.lifespan;
+  fill(this.colores[0],this.colores[1],this.colores[2]);
+  rect(this.position.x-tam/2,this.position.y-tam/2, tam, tam);
+
+  aux=tam-20;
+  if(aux<=0){
+    aux=0;
+  }
+  fill(this.colores[0],this.colores[1]-20, this.colores[2] -20);
+  rect(this.position.x-aux/2,this.position.y-aux/2, aux, aux);
+    
+  aux2=tam-40;
+  if(aux2<=0){
+    aux2=0;
+  }
+  fill(this.colores[0],this.colores[1]-40, this.colores[2] -40);
+  rect(this.position.x-aux2/2,this.position.y-aux2/2, aux2, aux2);
+  
+  //stroke(0);
+  //line(this.position.x, this.position.y, this.position.x-tam/2, this.position.y-tam/2);
+
+};
+
+
+// Is the particle still useful?
+Particle.prototype.isDead = function(){
+  return this.lifespan < 0;
+};
+
+let ParticleSystem = function(position) {
+  this.origin = position.copy();
+  this.particles = [];
+};
+
+ParticleSystem.prototype.addParticle = function(colores) {
+  this.particles.push(new Particle(this.origin,colores));
+};
+
+ParticleSystem.prototype.run = function() {
+  for (let i = this.particles.length-1; i >= 0; i--) {
+    let p = this.particles[i];
+    p.run();
+    if (p.isDead()) {
+      this.particles.splice(i, 1);
+    }
+  }
+};
